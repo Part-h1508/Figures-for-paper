6228a368b94d79896955b58ae141d0a458bbb988
Parth <kattiparth1.6@gmail.com>
2026-04-02 01:25:56 +0530

    Tweaked code

--- TAI/Autocorrelation_Limit_cycle_figure_12.py
+++ TAI/Autocorrelation_Limit_cycle_figure_12.py
@@ -1,84 +1,64 @@
 """
-this file is to simulate autocorrelation for TAI
+this file is to simulate autocorrelation for TAI (original method)
 the graph describes temporal behaviour of pressure signal
 
-updated:
---> using Excel CORREL style (pearson correlation)
---> using short segment (2 sec)
---> removed unstable high lag region
+we use FFT-based autocorrelation (faster than np.correlate)
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+from scipy import signal
 
 # variables
 folder_path = "TAI"
 
 # mapping files to Reynolds number
 re_map = {
-    15: 1565.85, 
-    20: 2087.80, 
-    25: 2609.75, 
-    30: 3131.70, 
-    35: 3653.65, 
+    15: 1565.85,  
     40: 4175.60, 
-    45: 4697.55, 
-    50: 5219.50, 
     53: 5532.67
 }
 
 plt.figure(figsize=(10, 6))
 
+# sampling frequency (as used in experiment)
+fs = 44100
+samples_needed = int(2 * fs)  # 2 seconds
+max_lag = int(0.02 * fs)  # 20 ms
+
 for file_num, re_val in re_map.items():
 
     file_name = os.path.join(folder_path, f"{file_num}.xlsx")
     
-    df = pd.read_excel(file_name, header=None)
-    signal = df[0].to_numpy()
+    # Read only the first 2 seconds of data
+    df = pd.read_excel(file_name, header=None, nrows=samples_needed)
+    signal_data = df[0].to_numpy()
 
-    # sampling frequency (assume uniform time step)
-    # if time column exists, use that instead
-    fs = 44100  # keep same as your experiment
+    # remove mean
+    signal_data = signal_data - np.mean(signal_data)
 
-    # use only first 2 seconds
-    signal = signal[:int(2 * fs)]
+    # autocorrelation using FFT (much faster than np.correlate)
+    corr = signal.correlate(signal_data, signal_data, mode='same', method='fft')
+    center = len(corr) // 2
 
-    # autocorrelation
-    acf = []
+    # normalize by peak (at lag 0)
+    corr = corr / corr[center]
 
-    max_lag = 15
-    min_points = 50
+    # extract only the lag window we need (20 ms)
+    rxx = corr[center:center + max_lag]
 
-    for k in range(max_lag + 1):
+    lag = (np.arange(max_lag) / fs) * 1000
 
-        if k == 0:
-            x1 = signal
-            x2 = signal
-        else:
-            x1 = signal[:-k]
-            x2 = signal[k:]
+    plt.plot(lag, rxx, label=f"Re = {re_val:.0f}")
 
-        if len(x1) < min_points:
-            break
-
-        r = np.corrcoef(x1, x2)[0, 1]
-        acf.append(r)
-
-    acf = np.array(acf)
-    acf = np.nan_to_num(acf)
-
-    lag = np.arange(len(acf))
-
-    plt.plot(lag, acf, label=f"Re = {re_val:.0f}")
-
-plt.xlabel("Lag")
+plt.xlabel("Lag Time (ms)")
 plt.ylabel("Autocorrelation Function")
 
 plt.grid(True, alpha=0.3)
 plt.legend()
 
 plt.tight_layout()
-plt.savefig("Figure_TAI_Autocorrelation_ex.png", dpi=300)
+plt.savefig("Figure_TAI_Autocorrelation_original_1.png", dpi=300)
 plt.show()
